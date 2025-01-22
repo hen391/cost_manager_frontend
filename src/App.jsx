e81da82309b757da22bbcb048dceac95df01f0d0
GalTouti <toutigal@gmail.com>
2025-01-22 17:40:07 +0200

    import react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddCostForm from './components/AddCostForm';
+import PieChart from './components/PieChart';
 
 function App() {
     return (
         <div>
             <h1>Cost Manager Frontend</h1>
             <AddCostForm />
+            <PieChart/>
         </div>
     );
 }
