2635f3fc7279ca07e96f1b1ccb6186e1321e90d4
hen391 <hen391@gmail.com>
2025-02-09 12:42:25 +0200

    last fixes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
